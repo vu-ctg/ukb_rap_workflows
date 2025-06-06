26ec83ea875137d3d5ff40bc5fb7a0f47ba75e53
jesavage <j.e.savage@vu.nl>
2025-06-06 20:46:37 +0200

    Update and rename a_plink_convert_bgen_pgen.sh to gwas_step1.sh

--- gwas_analyses/gwas_step1.sh
+++ gwas_analyses/gwas_step1.sh
@@ -3,8 +3,9 @@
 #For CTG pipeline, we keep individuals that are defined in the file `unrel_EUR.ids` and SNPs in the file `snplist_QCinclude_rsid.txt`
 
 projectid= #put in projectID
-shared_files_path= #put in the path for shared files
-path= #put in the path to store outfiles
+ctgprojectid=project-GgbZPkjJ2JG1kB92GKyB7Zb5 #assumems membership in CTG shared project
+path= #put in the path in user's project to store output files
+ancestry="EUR" #specify ancesty group for analysis, one of AFR,AMR,EAS,EUR,SAS
 
 for chr in {1..22}
 do
@@ -12,18 +13,18 @@ do
 run_plink_cmd="plink2 --bgen ukb22828_c${chr}_b0_v3.bgen ref-first \
         --sample ukb22828_c${chr}_b0_v3.sample \
         --make-pgen \
-        --keep unrel_EUR.ids \
+        --keep unrel_${ancestry}.ids \
         --extract snplist_QCinclude_rsid.txt \
         --hard-call-threshold 0.1 \
         --out ukb22828_c${chr}_b0_v3_filtered"
 
 dx run app-swiss-army-knife \
--iin=${projectid}:/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c${chr}_b0_v3.bgen \
--iin=${projectid}:/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c${chr}_b0_v3.sample \
--iin=${projectid}:${shared_files_path}/unrel_EUR.ids \
--iin=${projectid}:${shared_files_path}/snplist_QCinclude_rsid.txt \
+-iin=${ctgprojectid}:/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c${chr}_b0_v3.bgen \
+-iin=${ctgprojectid}:/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c${chr}_b0_v3.sample \
+-iin=${ctgprojectid}:/Subject\ Lists/unrel_${ancestry}.ids \
+-iin=${projectid}:/Variant\ Lists/snplist_QCinclude_rsid.txt \
 -icmd="${run_plink_cmd}" \
---instance-type mem1_ssd1_v2_x8 \
+--instance-type mem2_ssd1_v2_x8 \
 --name chr${chr}_bgen_pgen \
 --destination ${projectid}:${path} \
 --priority normal \
