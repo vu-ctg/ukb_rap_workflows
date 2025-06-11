2b693be15da88b79811f32c8de21e0022b3c1411
jesavage <j.e.savage@vu.nl>
2025-06-11 15:09:51 +0200

    Update gwas_single_step.sh

--- gwas_analyses/gwas_single_step.sh
+++ gwas_analyses/gwas_single_step.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# run GWAS in parallel across chromosomes with default settings + EUR ancestry
-# QC and analysis per chr combined in a single job
+# run GWAS in a single step (QC+analysis), in parallel across chromosomes 
+#For CTG pipeline, we keep individuals that are defined in the file `unrel_EUR.ids` and SNPs in the file `snplist_QCinclude_rsid.txt`
+
+projectid= #put in user's projectID
+ctgprojectid=project-GgbZPkjJ2JG1kB92GKyB7Zb5 #assumems membership in CTG shared project
+path= #put in the path in user's project to store output files
+ancestry="EUR" #specify ancesty group for analysis, one of AFR,AMR,EAS,EUR,SAS
+pheno= #put in the basename of the {phenotype}.pheno, example: gestational_diabetes. This assume that there is a file gestational_diabetes.pheno
+covar="gwa_covs.txt" #put in the path to the covariate file; default is CTG shared file 
+
+
 
 for i in {1..22} X XY; 
 do
@@ -9,20 +18,25 @@ do
 run_plink_cmd="echo 'Convert bgen to pgen'; 
  
 plink2 --bgen ukb22828_c${i}_b0_v3.bgen ref-first \
---sample ukb22828_c${i}_b0_v3.sample --make-pgen \
---keep unrel_EUR.ids --extract snplist_QCinclude_rsid.txt \
---memory 70000 --threads 36 \
---hard-call-threshold 0.1 --out ukb22828_c${i}_b0_v3_filtered;
+--sample ukb22828_c${i}_b0_v3.sample \
+--make-pgen \
+--keep unrel_${ancestry}.ids \
+--extract snplist_QCinclude_rsid.txt \
+--memory 64000 --threads 16 \
+--hard-call-threshold 0.1 \
+--out ukb22828_c${i}_b0_v3_filtered;
 
 echo 'Run analysis for chr ${i}'; 
 
 plink2 --pfile ukb22828_c${i}_b0_v3_filtered \
 --maf 0.0001 --geno 0.05 \
---covar-variance-standardize --glm \
---covar rap_test.covs --covar-name sex,age,array01,pop_pc1-pop_pc20 \
---pheno rap_test.pheno --pheno-name alcquant \
---memory 70000 --threads 36 \
---out c${i}_alcquant; 
+--glm firth-fallback no-x-sex hide-covar \
+--covar ${covar} \
+--covar-name sex,age,array01,pop_pc1-pop_pc20 \
+--covar-variance-standardize \
+--pheno ${pheno}.pheno \
+--memory 64000 --threads 16 \
+--out c${chr}_${pheno}_${ancestry}; 
 
 echo 'clean up workspace';
 
@@ -33,16 +47,18 @@ echo 'done' "
 
 
 dx run app-swiss-army-knife \
--iin=project-GgbZPkjJ2JG1kB92GKyB7Zb5:/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c${i}_b0_v3.bgen \
--iin=project-GgbZPkjJ2JG1kB92GKyB7Zb5:/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c${i}_b0_v3.sample \
--iin=project-GgbZPkjJ2JG1kB92GKyB7Zb5:/benchmark/rap_test.covs \
--iin=project-GgbZPkjJ2JG1kB92GKyB7Zb5:/benchmark/rap_test.pheno \
--iin=project-GgbZPkjJ2JG1kB92GKyB7Zb5:/Subject\ Lists/unrel_EUR.ids \
--iin=project-GgbZPkjJ2JG1kB92GKyB7Zb5:/Variant\ Lists/snplist_QCinclude_rsid.txt \
+-iin=${ctgprojectid}:/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c${chr}_b0_v3.bgen \
+-iin=${ctgprojectid}:/Bulk/Imputation/UKB\ imputation\ from\ genotype/ukb22828_c${chr}_b0_v3.sample \
+-iin=${ctgprojectid}:/Subject\ Lists/unrel_${ancestry}.ids \
+-iin=${projectid}:/Variant\ Lists/snplist_QCinclude_rsid.txt \
+-iin=${projectid}:${path}/${pheno}.pheno \
+-iin=${ctgprojectid}:${path}/${covar} \
 -icmd="${run_plink_cmd}" \
---instance-type mem1_ssd1_v2_x36 \
---name gwas_chr${i} \
---cost-limit 2 --priority low -y \
---destination project-GgbZPkjJ2JG1kB92GKyB7Zb5:/benchmark/plink_workflow
+--instance-type mem2_ssd1_v2_x16 \
+--name gwas_${pheno}_${ancestry}_chr${i} \
+--destination ${projectid}:${path} \
+--cost-limit 1 \
+--priority low \
+-y 
 
 done
