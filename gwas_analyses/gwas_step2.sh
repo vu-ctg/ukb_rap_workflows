285de13cbf9a3842e6acac4eef20f4f0b8470b68
jesavage <j.e.savage@vu.nl>
2026-04-01 21:35:35 +0200

    Update gwas_step2.sh

--- gwas_analyses/gwas_step2.sh
+++ gwas_analyses/gwas_step2.sh
@@ -3,37 +3,38 @@
 
 projectid= #put in user's projectID
 ctgprojectid=project-GgbZPkjJ2JG1kB92GKyB7Zb5 #assumes membership in CTG shared project
-path= #put in the path in user's project to store output files
+path= #put in the path in user's project to store output files (excluding the leading "/" )
 ancestry="EUR" #specify ancesty group for analysis, one of AFR,AMR,EAS,EUR,SAS
-pheno= #put in the basename of the {phenotype}.pheno, example: gestational_diabetes. This assume that there is a file gestational_diabetes.pheno
+pheno= #put in the basename of the {phenotype}.pheno input file, e.g.: "pheno=gestational_diabetes" 
+      ## where a file "gestational_diabetes.pheno" exists in the users's project directory
 covar="gwa_covs.txt" #put in the path to the covariate file; default is CTG shared file 
 
-for chr in {1..22}; 
+for chr in {1..22} 
 do
 
-
 run_plink_cmd="plink2 --pfile ukb22828_c${chr}_b0_v3_filtered \
-      --maf 0.0001 --geno 0.05 \
+      --maf 0.0001 \
+      --geno 0.05 \
       --glm firth-fallback no-x-sex hide-covar \
       --pheno ${pheno}.pheno \
       --covar ${covar} \
       --covar-name sex,age,array01,pop_pc1-pop_pc20 \
       --covar-variance-standardize \
-      --memory 64000 --threads 16 \    
+      --memory 64000 \
       --out c${chr}_${pheno}_${ancestry}"
 
-
 dx run app-swiss-army-knife \
--iin=${projectid}:${path}/ukb22828_c${chr}_b0_v3_filtered.pgen \
--iin=${projectid}:${path}/ukb22828_c${chr}_b0_v3_filtered.psam \
--iin=${projectid}:${path}/ukb22828_c${chr}_b0_v3_filtered.pvar \
--iin=${projectid}:${path}/${pheno}.pheno \
--iin=${projectid}:${path}/${covar} \
+-iin=${projectid}:/${path}/ukb22828_c${chr}_b0_v3_filtered.pgen \
+-iin=${projectid}:/${path}/ukb22828_c${chr}_b0_v3_filtered.psam \
+-iin=${projectid}:/${path}/ukb22828_c${chr}_b0_v3_filtered.pvar \
+-iin=${projectid}:/${path}/${pheno}.pheno \
+-iin=${ctgprojectid}:/${covar} \
 -icmd="${run_plink_cmd}" \
 --instance-type mem1_ssd1_v2_x16 \
 --name chr${chr}_gwas_plink \
---destination ${projectid}:${path} \
+--destination ${projectid}:/${path} \
 --priority low \
+--cost-limit 0.5 \
 --y
 
 done
